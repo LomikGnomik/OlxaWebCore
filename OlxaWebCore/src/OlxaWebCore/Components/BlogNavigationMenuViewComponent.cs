a66bcc005014b1ece875b4422f47f16eba567a2a
Unknown <lomik248@mail.ru>
2017-05-20 20:42:19 +0300

    порешал

---
+++ OlxaWebCore/src/OlxaWebCore/Components/BlogNavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlxaWebCore.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OlxaWebCore.Components
+{
+    public class BlogNavigationMenuViewComponent :ViewComponent
+    {
+        private IBlogRepository repository;
+
+        public BlogNavigationMenuViewComponent(IBlogRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Posts
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
