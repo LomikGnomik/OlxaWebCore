158a4311014740007c7ef1b98e3504e75b12e8d6
Oli4 <5-batonov@mail.ru>
2017-06-09 12:10:06 +0300

    Аккордеон и Этапы разработки

--- OlxaWebCore/src/OlxaWebCore/wwwroot/js/scripts.js
+++ OlxaWebCore/src/OlxaWebCore/wwwroot/js/scripts.js
@@ -1,180 +1,56 @@
-﻿/*----------------------------------------"этапы разработки----------------------------*/
-var allSlides, slide1, slide2, slide3, slide4, allSubnavs;
+﻿/*----------------------------------------этапы разработки----------------------------*/
+jQuery(document).ready(function ($) {
 
-allSlides = $('.slide');
-slide1 = $('#slide-1');
-slide2 = $('#slide-2');
-slide3 = $('#slide-3');
-slide4 = $('#slide-4');
+    $('#steps > div').click(function () {
 
-allSubnavs = $('.slide-nav');
+        var i = $(this).attr('data-step') - 1;
 
-// Next-Prev navigation for slider
-$('.next').on('click', function (e) {
-    e.preventDefault();
+        $('#steps > div').each(function (index) {
+            if (index < i) {
+                $(this).addClass('done');
+                $(this).removeClass('active');
+            } else if (index == i) {
+                $(this).addClass('active');
+                $(this).removeClass('done');
+            } else {
+                $(this).removeClass('done');
+                $(this).removeClass('active');
+            }
+        });
 
-    // Sub-nav buttons have to slide too
-    nextSlideNav = $('.active-slide.slide-nav').next('div');
-    if (nextSlideNav.length == 0) {
-        nextSlideNav = $('.slide-nav:first');
-    }
-    nextSlideNav.siblings().removeClass('active-slide');
-    nextSlideNav.addClass('active-slide');
+        $('article').each(function (index) {
+            if (index < i - 1) {
+                clearClasses($(this));
+                $(this).addClass('active_m_2');
+            } else if (index > i + 1) {
+                clearClasses($(this));
+                $(this).addClass('active_p_2');
+            } else if (index < i) {
+                clearClasses($(this));
+                $(this).addClass('active_m_1');
+            } else if (index > i) {
+                clearClasses($(this));
+                $(this).addClass('active_p_1');
+            } else {
+                clearClasses($(this));
+                $(this).addClass('active');
+            }
+        });
 
-    // Slides
-    currentSlide = $('.active-slide.slide');
-    currentSlide.addClass('recent-slide');
+        function clearClasses(item) {
+            var a = [
+              "active_m_2",
+              "active_m_1",
+              "active",
+              "active_p_1",
+              "active_p_2"
+            ];
+            a.forEach(function (class_name) {
+                item.removeClass(class_name);
+            });
+        }
 
-    nextSlide = $('.active-slide.slide').next('.slide');
-    if (nextSlide.length == 0) {
-        nextSlide = $('.slide:first');
-    }
-
-    allSlides.removeClass('active-slide');
-    nextSlide.removeClass('move-right move-left').addClass('active-slide');
-
-    setTimeout(function () {
-        $(nextSlide).prevAll('.slide').removeClass('move-right').addClass('move-left');
-        $(nextSlide).nextAll('.slide').removeClass('move-left').addClass('move-right');
-
-    }, 250);
-
-    setTimeout(function () {
-        currentSlide.removeClass('recent-slide');
-    }, 610);
-
-});
-
-$('.prev').on('click', function (e) {
-    e.preventDefault();
-
-    // Sub-nav buttons have to slide too
-    prevSlideNav = $('.active-slide.slide-nav').prev('div');
-    if (prevSlideNav.length == 0) {
-        prevSlideNav = $('.slide-nav:last');
-    }
-    prevSlideNav.siblings().removeClass('active-slide');
-    prevSlideNav.addClass('active-slide');
-
-    // Slides
-    currentSlide = $('.active-slide.slide');
-    currentSlide.addClass('recent-slide');
-
-    prevSlide = $('.active-slide.slide').prev('.slide');
-    if (prevSlide.length == 0) {
-        prevSlide = $('.slide:last');
-    }
-
-    allSlides.removeClass('active-slide');
-    prevSlide.removeClass('move-right move-left').addClass('active-slide');
-
-    setTimeout(function () {
-        $(prevSlide).prevAll('.slide').removeClass('move-right').addClass('move-left');
-        $(prevSlide).nextAll('.slide').removeClass('move-left').addClass('move-right');
-    }, 250);
-
-    setTimeout(function () {
-        currentSlide.removeClass('recent-slide');
-    }, 610);
-
-});
-
-// Sub-nav for slider
-$('.one').on('click', function (e) {
-    e.preventDefault();
-    // 	Sub-nav
-    currentSlideNav = $(this).closest('div');
-    currentSlideNav.siblings().removeClass('active-slide');
-    currentSlideNav.addClass('active-slide');
-
-    // 	Slides
-    currentSlide = $('.active-slide.slide');
-    currentSlide.addClass('recent-slide');
-
-    $(allSlides).removeClass('active-slide');
-    $(slide1).removeClass('move-left').addClass('active-slide');
-
-    setTimeout(function () {
-        $(slide1).nextAll('.slide').removeClass('move-left').addClass('move-right');
-    }, 250);
-
-    setTimeout(function () {
-        currentSlide.removeClass('recent-slide');
-    }, 610);
-
-});
-
-$('.two').on('click', function (e) {
-    e.preventDefault();
-    // 	Sub-nav
-    currentSlideNav = $(this).closest('div');
-    currentSlideNav.siblings().removeClass('active-slide');
-    currentSlideNav.addClass('active-slide');
-
-    // 	Slides
-    currentSlide = $('.active-slide.slide');
-    currentSlide.addClass('recent-slide');
-
-    $(allSlides).removeClass('active-slide');
-    $(slide2).removeClass('move-right move-left').addClass('active-slide');
-
-    setTimeout(function () {
-        $(slide2).prevAll('.slide').removeClass('move-right').addClass('move-left');
-        $(slide2).nextAll('.slide').removeClass('move-left').addClass('move-right');
-    }, 250);
-
-    setTimeout(function () {
-        currentSlide.removeClass('recent-slide');
-    }, 610);
-
-});
-
-$('.three').on('click', function (e) {
-    e.preventDefault();
-    // 	Sub-nav
-    currentSlideNav = $(this).closest('div');
-    currentSlideNav.siblings().removeClass('active-slide');
-    currentSlideNav.addClass('active-slide');
-
-    // 	Slides
-    currentSlide = $('.active-slide.slide');
-    currentSlide.addClass('recent-slide');
-
-    $(allSlides).removeClass('active-slide');
-    $(slide3).removeClass('move-right move-left').addClass('active-slide');
-
-    setTimeout(function () {
-        $(slide3).prevAll('.slide').removeClass('move-right').addClass('move-left');
-        $(slide3).nextAll('.slide').removeClass('move-left').addClass('move-right');
-    }, 250);
-
-    setTimeout(function () {
-        currentSlide.removeClass('recent-slide');
-    }, 610);
-
-});
-
-$('.four').on('click', function (e) {
-    e.preventDefault();
-    // 	Sub-nav
-    currentSlideNav = $(this).closest('div');
-    currentSlideNav.siblings().removeClass('active-slide');
-    currentSlideNav.addClass('active-slide');
-
-    // 	Slides
-    currentSlide = $('.active-slide.slide');
-    currentSlide.addClass('recent-slide');
-
-    $(allSlides).removeClass('active-slide');
-    $(slide4).removeClass('move-right').addClass('active-slide');
-
-    setTimeout(function () {
-        $(slide4).prevAll('.slide').removeClass('move-right').addClass('move-left');
-    }, 250);
-
-    setTimeout(function () {
-        currentSlide.removeClass('recent-slide');
-    }, 610);
+    });
 
 });
 /*----------------------------------------фиксированное меню---------------------------*/
