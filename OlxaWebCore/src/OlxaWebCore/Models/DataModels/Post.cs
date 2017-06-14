b711554a530e053e31a005741a2668d628bc83de
Unknown <lomik248@mail.ru>
2017-06-14 18:44:18 +0300

    мелкие мелочи

--- OlxaWebCore/src/OlxaWebCore/Models/DataModels/Post.cs
+++ OlxaWebCore/src/OlxaWebCore/Models/DataModels/Post.cs
@@ -10,15 +10,17 @@ namespace OlxaWebCore.Models.DataModels
     {
         public int PostID { get; set; }
 
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [Display(Name = "Заголовок")]
         public virtual string Title
         { get; set; }
 
-
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [Display(Name = "Короткое описание")]
         public virtual string ShortDescription
         { get; set; }
 
+        [Required]
         [Display(Name = "Описание")]
         public virtual string Description
         { get; set; }
@@ -35,6 +37,8 @@ namespace OlxaWebCore.Models.DataModels
         public virtual bool Published
         { get; set; }
 
+
+        [Required]
         [Display(Name = "Категория")]
         public virtual string Category
         { get; set; }
