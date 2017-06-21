97eee4dbd6e3e8721721fc9097e10a9a330d0558
Unknown <lomik248@mail.ru>
2017-06-21 13:49:25 +0300

    портфолио для каждого сайта

---
+++ OlxaWebCore/src/OlxaWebCore/Components/ExamplesSitesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlxaWebCore.Models.DataModels;
+using OlxaWebCore.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OlxaWebCore.Components
+{
+    // Примеры сайтов из портфолио соответствующие категории
+    public class ExamplesSitesViewComponent: ViewComponent
+    {
+        private IPortfolioRepository repository;
+
+        public ExamplesSitesViewComponent(IPortfolioRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke(string category)
+        {
+            IEnumerable<Portfolio> site = repository.Portfolios.Where(p => p.Published == true & p.Category == category);
+
+            return View(site);
+        }
+    }
+}
