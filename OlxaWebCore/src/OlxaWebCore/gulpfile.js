8bae49f5247e98a6fc2829abd5a69a724466457e
Oli4 <5-batonov@mail.ru>
2017-07-23 00:42:07 +0300

    LESS в CSS на лету

--- OlxaWebCore/src/OlxaWebCore/gulpfile.js
+++ OlxaWebCore/src/OlxaWebCore/gulpfile.js
@@ -6,7 +6,11 @@ var gulp = require("gulp"),
   less = require("gulp-less");
 
 gulp.task("less", function () {
-    return gulp.src('Styles/styles.less')
+    return gulp.src('Styles/*.less')
       .pipe(less())
       .pipe(gulp.dest('wwwroot/css'));
 });
+gulp.task('watch', function () {
+    gulp.watch('Styles/*.less', ['less']);
+
+})
